f9e7aa021af31128a2e1ebe0491f4647999414f0
chaerin <jcrs0907@gmail.com>
2022-04-11 18:06:43 +0900

    2022.04.11 add vanilla javascript shopping-mall

--- 1-vanilla/js/views/KeywordView.js
+++ 1-vanilla/js/views/KeywordView.js
@@ -1,40 +1,41 @@
 import View from './View.js'
 
-const tag = '[TabView]'
+const tag = '[KeywordView]'
 
 const KeywordView = Object.create(View)
 
-KeywordView.setup = function(el){
-  this.init(el);
-  this.bindClickEvent();
-  return this;
+KeywordView.messages = {
+  NO_KEYWORDS: '추천 검색어가 없습니다'
 }
 
-KeywordView.render = function(data = []){
-  this.el.innerHTML = data.length ? this.getKeywordsHtml(data) : '추천 검색어가 없습니다'
+KeywordView.setup = function (el) {
+  this.init(el)
+  return this
+}
+
+KeywordView.render = function (data = []) {
+  this.el.innerHTML = data.length ? this.getKeywordsHtml(data) : this.messages.NO_KEYWORDS
   this.bindClickEvent()
   this.show()
+  return this
 }
 
-//추천 검색어를 뿌려주는 메서드
-KeywordView.getKeywordsHtml = function(data){
-  return data.reduce((html, item, index) =>{
+KeywordView.getKeywordsHtml = function (data) {
+  return data.reduce((html, item, index) => {
     html += `<li data-keyword="${item.keyword}"><span class="number">${index + 1}</span>${item.keyword}</li>`
     return html
-  }, '<ul class="list">') + '</ul>'
+  }, '<ul class="list">') + "</ul>"
 }
 
-KeywordView.bindClickEvent = function(){
-  //li모음이 배열이 아닌 유사 배열이어서 Array.from을 사용해 array로 만들어준다.
-  Array.from(this.el.querySelectorAll('li')).forEach(li=>{
-    li.addEventListener('click', e => { this.onClickKeyword(e); });
-  });
+KeywordView.bindClickEvent = function () {
+  Array.from(this.el.querySelectorAll('li')).forEach(li => {
+    li.addEventListener('click', e => this.onClickKeyword(e))
+  })
 }
 
-KeywordView.onClickKeyword = function(e){
-  const {keyword} = e.currentTarget.dataset;
-  this.emit('@click', keyword);
-
+KeywordView.onClickKeyword = function (e) {
+  const { keyword } = e.currentTarget.dataset
+  this.emit('@click', { keyword })
 }
 
 export default KeywordView
--- 1-vanilla/js/views/ResultView.js
+++ 1-vanilla/js/views/ResultView.js
@@ -1,25 +1,26 @@
 import View from './View.js';
 
-const tag = '[ResertView]'
+const tag = '[ResultView]'
 
 //Object.create란?
-const ResertView = Object.create(View);
+const ResultView = Object.create(View);
 
-//ResertView 관련 메세지 모음
-ResertView.messages = {
+//ResultView 관련 메세지 모음
+ResultView.messages = {
   NO_RESULT : '검색 결과가 없습니다'
 }
 
-ResertView.setup = function(el){
+ResultView.setup = function(el){
   this.init(el);
 }
 //메인컨트롤러에서 render 함수를 호출해야 한다.
-ResertView.render = function(data  = []){
+ResultView.render = function(data  = []){
   console.log(tag, 'render()', data);
   this.el.innerHTML = data.length ? this.getSearchResultHtml(data) : this.messages.NO_RESULT
+  this.show()
 }
 
-ResertView.getSearchResultHtml = function(data){
+ResultView.getSearchResultHtml = function(data){
   //html 문자열 리턴용 함수
   //data가 컬렉션이어서 reduce 함수 이용
   return data.reduce((html, item) => {
@@ -30,11 +31,11 @@ ResertView.getSearchResultHtml = function(data){
 }
 
 //검색결과 안에 하나의 아이템 image, name을 넣어서 리턴
-ResertView.getSearchItemHtml = function(item){
+ResultView.getSearchItemHtml = function(item){
 return `<li>
   <img src="${item.image}">
   <p>${item.name}</p>
 </li>`
 }
 
-export default ResertView
+export default ResultView
